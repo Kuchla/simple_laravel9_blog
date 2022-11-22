e46cac5d495eaca1adc887c1d4bc621be9c89539
kuchla <ravana@DESKTOP-1JGVFIE.localdomain>
2022-11-22 17:38:18 -0300

    Created category model with seeder and factory

---
+++ database/factories/CategoryFactory.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+use App\Models\Category;
+use App\Models\User;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Category>
+ */
+class CategoryFactory extends Factory
+{
+    protected $model = Category::class;
+
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition()
+    {
+        return [
+            'name' => $this->faker->word(),
+            'active' => $this->faker->boolean(),
+            'user_id' => User::first()->id
+        ];
+    }
+}
--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -10,6 +10,7 @@ use Illuminate\Support\Str;
  */
 class UserFactory extends Factory
 {
+    public $email = 'admin@admin.com';
     /**
      * Define the model's default state.
      *
@@ -17,13 +18,16 @@ class UserFactory extends Factory
      */
     public function definition()
     {
-        return [
-            'name' => fake()->name(),
-            'email' => fake()->unique()->safeEmail(),
+        $user = [
+            'name' => $this->faker->name(),
+            'email' => $this->email,
             'email_verified_at' => now(),
-            'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
+            'password' => bcrypt("123123123"),
             'remember_token' => Str::random(10),
         ];
+
+        $this->email = $this->faker->email();
+        return $user;
     }
 
     /**
